4399d8fa1cf2d5512adfb3ee0db999bbb5015c87
mumbereh <siirasonh@gmail.com>
2024-09-29 20:26:37 +0300

    udated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  config.assets.js_compressor = :uglifier i
+  config.assets.js_compressor = :uglifier 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
